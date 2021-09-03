85b01892c455020d81c3baafb2c1bbe62c747a22
7benson <jknani111@gmail.com>
2021-09-04 00:24:19 +0530

    shell script

--- lab2/dedup/dedup.sh
+++ lab2/dedup/dedup.sh
@@ -1,1 +1,8 @@
-# TODO: Write your shell script (for question 7) in this file.
+for file1 in $(find . -name '*.csv'); do
+    for file2 in $(find . -name '*.csv'); do
+    	if diff -q "${file1}" "${file2}" >/dev/null && "$file1" != "$file2" ; then
+    		"$(ln -s "${file1}" "${file2}")"
+    		#"$(rm "${file2}")"
+    	fi
+    done
+done
